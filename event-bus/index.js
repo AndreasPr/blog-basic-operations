59ff90c249669848bd8172edccad9e4b9dde94d4
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-04-24 17:18:41 -0400

    changes to event-bus service

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4001/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4002/events', event).catch((err) => {console.log(err.message)});
-    axios.post('http://localhost:4003/events', event).catch((err) => {console.log(err.message)});
+    axios.post('http://localhost:4000/events', event).catch((err) => {console.log(err.message)}); //Posts Service
+    axios.post('http://localhost:4001/events', event).catch((err) => {console.log(err.message)}); //Comments Service
+    axios.post('http://localhost:4002/events', event).catch((err) => {console.log(err.message)}); //Query Service
+    axios.post('http://localhost:4003/events', event).catch((err) => {console.log(err.message)}); //Moderation Service
 
     res.send({status: 'OK'});
 });
