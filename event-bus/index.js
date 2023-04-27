7fb809bdabf4ba9bc78b989fbb895b6d66904756
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2023-04-26 20:12:34 -0400

    Updated service addresses in order pods(posts and event-bus) to communicate each other

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,7 +12,7 @@ app.post('/events', (req, res) => {
 
     events.push(event);
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {console.log(err.message)}); //Posts Service
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((err) => {console.log(err.message)}); //Posts Service
     axios.post('http://localhost:4001/events', event).catch((err) => {console.log(err.message)}); //Comments Service
     axios.post('http://localhost:4002/events', event).catch((err) => {console.log(err.message)}); //Query Service
     axios.post('http://localhost:4003/events', event).catch((err) => {console.log(err.message)}); //Moderation Service
